ff8f8aadac3bf8c207a7e7e4611124c0a2881626
Zach Sanderson <zach@zachsanderson.com>
2016-05-16 15:22:43 -0500

    Adding lesson folders but not node_modules

---
+++ 06_04/api.js
@@ -0,0 +1,36 @@
+var http = require("http");
+
+var data = require("./data/inventory");
+
+http.createServer(function(req, res) {
+
+    if (req.url === "/") {
+        res.writeHead(200, {"Content-Type": "text/json"});
+        res.end(JSON.stringify(data));
+    } else if (req.url === "/instock") {
+        listInStock(res);
+    } else if (req.url === "/onorder") {
+        listOnBackOrder(res);
+    } else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("Whoops... data not found");
+    }
+}).listen(3000);
+
+console.log("server listening on port 3000");
+
+function listInStock(res) {
+    var inStock = data.filter(function(item) {
+        return item.avail === "In stock";
+    });
+
+    res.end(JSON.stringify(inStock));
+}
+
+function listOnBackOrder(res) {
+    var onOrder = data.filter(function(item) {
+        return item.avail === "On back order";
+    });
+
+    res.end(JSON.stringify(onOrder));
+}
