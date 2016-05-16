6d331f369e8e18a32e6790afcff5aa5dd0fa62b7
Zach Sanderson <zach@zachsanderson.com>
2016-05-16 15:33:16 -0500

    Finished 10_05 - Testing with Sinon spies

--- 10_05/test/order-spec.js
+++ 10_05/test/order-spec.js
@@ -3,6 +3,8 @@ var rewire = require("rewire");
 
 var order = rewire("../lib/order");
 
+var sinon = require("sinon");
+
 describe("Ordering Items", function() {
 
 	beforeEach(function() {
@@ -13,13 +15,21 @@ describe("Ordering Items", function() {
 			{sku: "CCC", qty: 3}
 		];
 
+		this.console = {
+			log: sinon.spy()
+		};
+
 		order.__set__("inventoryData", this.testData);
+		order.__set__("console", this.console);
 
 	});
 
 	it("order an item when there are enough in stock", function(done) {
 
+		var _this = this;
+
 		order.orderItem("CCC", 3, function() {
+			expect(_this.console.log.callCount).to.equal(1);
 			done();
 		});
 
